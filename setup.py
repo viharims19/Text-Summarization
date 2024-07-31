d3ff502cc61ceec83b4bfbfe218fc5c85dd76053
viharims19 <sai.vihari95@gmail.com>
2024-07-31 21:44:48 +0530

    Add Requirements and Install in venv

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'text_summarizer',
+    version= '0.0.0',
+    author= 'vihari',
+    author_email= 'sai.vihari95@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
