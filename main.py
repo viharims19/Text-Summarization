fb56ff297a91a8a9b4533a4bcb4cc8d94ba5bf3f
viharims19 <sai.vihari95@gmail.com>
2024-08-01 18:26:23 +0530

    Add DataIngestion

---
+++ main.py
@@ -0,0 +1,13 @@
+from TxtSmrzr.pipeline.training_pipeline import DataIngestionTrainingPipeline
+
+from TxtSmrzr.logging import logger
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
