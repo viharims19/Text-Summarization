3638cede2eb7ae0d7a8b13e52283c07df8d169d7
viharims19 <sai.vihari95@gmail.com>
2024-08-01 22:32:53 +0530

    Add DataTransformation,Model Trainning and Evaluation

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import sys
 import os
 
 from TxtSmrzr.pipeline.training_pipeline import (DataIngestionTrainingPipeline,
-                                                 DataValidationTrainingPipeline)
+                                                 DataValidationTrainingPipeline,
+                                                 DataTransformationTrainingPipeline,
+                                                 ModelTrainerTrainingPipeline,
+                                                 ModelEvaluationTrainingPipeline)
 
 
 from TxtSmrzr.logging import logger
@@ -22,6 +25,38 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/TxtSmrzr/pipeline/training_pipeline.py
+++ src/TxtSmrzr/pipeline/training_pipeline.py
@@ -1,6 +1,9 @@
 from TxtSmrzr.config.configuration import ConfigurationManager
 from TxtSmrzr.components.data_ingestion import DataIngestion
 from TxtSmrzr.components.data_validation import DataValidation
+from TxtSmrzr.components.data_transformation import DataTransformation
+from TxtSmrzr.components.model_trainer import ModelTrainer
+from TxtSmrzr.components.model_evaluation import ModelEvaluation
 from TxtSmrzr.logging import logger
 
 
@@ -24,3 +27,33 @@ class DataValidationTrainingPipeline:
         data_validation_config = config.get_data_validation_config()
         data_validation = DataValidation(config=data_validation_config)
         data_validation.validate_all_files_exist()
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
+class ModelEvaluationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
